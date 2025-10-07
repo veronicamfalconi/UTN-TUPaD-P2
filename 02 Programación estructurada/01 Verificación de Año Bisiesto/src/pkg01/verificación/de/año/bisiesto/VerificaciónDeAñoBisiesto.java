801f4e6e3218bca1ec947f0f1e776ab548ca21ec
Veronica Falconi <veronicamfalconi@gmail.com>
2025-10-07 10:23:48 -0300

    Primer punto del TP 01

---
+++ 02 Programación estructurada/01 Verificación de Año Bisiesto/src/pkg01/verificación/de/año/bisiesto/VerificaciónDeAñoBisiesto.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg01.verificación.de.año.bisiesto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Veronica
+ * Escribe un programa en Java que solicite al usuario un año y determine si es
+ * bisiesto. Un año es bisiesto si es divisible por 4, pero no por 100, salvo que sea
+ * divisible por 400.
+ * Ejemplo de entrada/salida:
+ * Ingrese un año: 2024
+ * El año 2024 es bisiesto.
+ * Ingrese un año: 1900
+ * El año 1900 no es bisiesto.
+
+ */
+public class VerificaciónDeAñoBisiesto {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int anioBisiesto;
+        boolean esBisiesto;
+        
+        System.out.println("Ingrese un año para verificar si es bisiesto: ");
+        anioBisiesto = Integer.parseInt(input.nextLine());
+        esBisiesto = true;
+        
+        if (anioBisiesto % 4 == 0) {
+            if (anioBisiesto % 100 == 0) {
+                if(anioBisiesto % 400 == 0) {
+                    esBisiesto = true;
+                } else {
+                    esBisiesto = false;
+                }
+            } else {
+                esBisiesto = true;
+            }
+        } else {
+            esBisiesto = false;
+        }
+        
+        if (esBisiesto ==  true) {
+            System.out.println("El año " + anioBisiesto + " es bisiesto.");
+        } else {
+            System.out.println("El año " + anioBisiesto + " no es bisiesto.");
+        }
+
+        
+    } //cierre de main
+    
+} //cierre de clase
