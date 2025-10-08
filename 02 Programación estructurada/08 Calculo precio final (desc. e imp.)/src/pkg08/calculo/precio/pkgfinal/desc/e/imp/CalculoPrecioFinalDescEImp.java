7c65f176fdaf1450074274f768078ff6671fe46f
Veronica Falconi <veronicamfalconi@gmail.com>
2025-10-08 05:17:39 -0300

    Octavo ejercicio TP2

--- 02 Programación estructurada/08 Calculo precio final (desc. e imp.)/src/pkg08/calculo/precio/pkgfinal/desc/e/imp/CalculoPrecioFinalDescEImp.java
+++ 02 Programación estructurada/08 Calculo precio final (desc. e imp.)/src/pkg08/calculo/precio/pkgfinal/desc/e/imp/CalculoPrecioFinalDescEImp.java
@@ -4,6 +4,8 @@
  */
 package pkg08.calculo.precio.pkgfinal.desc.e.imp;
 
+import java.util.Scanner;
+
 /**
  *8. Cálculo del Precio Final con impuesto y descuento.
 Crea un método calcularPrecioFinal(double impuesto, double
@@ -28,7 +30,29 @@ public class CalculoPrecioFinalDescEImp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        double precioBase, impuesto, descuento, precioFinal = 0, impuestoPorcentaje, descuentoPorcentaje;
+        
+        System.out.println("Ingrese el precio base: ");
+        precioBase = Double.parseDouble(input.nextLine());
+        
+        System.out.println("Ingrese el impuesto en porcentaje. Ejemplo: 10 para 10%");
+        impuesto = Double.parseDouble(input.nextLine());
+        impuestoPorcentaje = impuesto/100;
+        
+        System.out.println("Ingrese el descuento en porcentaje. Ejemplo: 10 para 10%");
+        descuento = Double.parseDouble(input.nextLine());
+        descuentoPorcentaje = descuento/100;
+
+        
+        precioFinal = calcularPrecioFinal(precioBase, impuestoPorcentaje, descuentoPorcentaje);
+        System.out.println("El precio final del producto es: " + precioFinal);
+    } //cierre del main
+    
+    static double calcularPrecioFinal (double precio, double impuesto, double descuento) {
+        double precioFinal;
+        precioFinal = precio + (precio*impuesto) - (precio*descuento);
+        return precioFinal;
     }
     
-}
+} //cierre de clase
